72d006e0bd4829add0f1f5290f9631eaaaad0394
alejandro <jaliyodiaz96@gmail.com>
2020-09-18 10:42:30 +0200

    changes 0,2,2

--- proyecto1/codigo.js
+++ proyecto1/codigo.js
@@ -1,2 +1,2 @@
 alert("hola mundo perraass")
-document.write("<label background-color='red'> Esto es un color rojo puteros </label>")
+document.write("<label background-color='red'> esto va con un document.write en un .js bitches</label>")
