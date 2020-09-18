1e2c7ec5888d395027fa11ddd60e624e0167742b
alejandro <jaliyodiaz96@gmail.com>
2020-09-18 11:05:44 +0200

    changes 0,2,3

--- proyecto1/codigo.js
+++ proyecto1/codigo.js
@@ -1,2 +1,10 @@
+
+/*
 alert("hola mundo perraass")
 document.write("<label background-color='red'> esto va con un document.write en un .js bitches</label>")
+ */
+
+ function accion(){
+ 	myObject = document.getElementById('cajaTexto');
+ 	alert(myObject.value);
+ }
